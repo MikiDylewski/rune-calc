c3729e1a5a3364a93c4c38f9328bc0a914c7bf79
Haqz <contact@haqz.org>
2024-11-09 01:39:23 +0100

    Init

---
+++ src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="container mx-auto bg-black">asd</div>
+</template>
+
+<script>
+// @ is an alias to /src
+export default {
+  name: "HomeView",
+  components: {},
+};
+</script>
